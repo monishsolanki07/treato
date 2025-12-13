59b8c1c0d4bce9f30b9852e0f469369424449830
monish <monishsolanki100@gmail.com>
2025-12-13 20:56:34 +0530

    initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return <h1>Treato Frontend</h1>
+}
+
+export default App
